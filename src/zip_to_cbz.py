ed80e7df7598b87df0a9572dc88bf1c2442abe7a
Alex Smirnov <97alexsmirnov@gmail.com>
2023-01-02 17:35:47 -0800

    Add capability to process multiple folders with the same run of the script

--- src/zip_to_cbz.py
+++ src/zip_to_cbz.py
@@ -7,7 +7,7 @@ import comicinfo
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("path", help="The path to the Calibre library or a specific subfolder of that library. \
+    parser.add_argument("paths", nargs="+", help="The path to the Calibre library or multiple paths to a specific subfolders of that library. \
                                         The script will recursively operate on all zip files that also have a \
                                         metadata.opf file in the same folder.")
     parser.add_argument("--comicinfo-schema-version", "--schema", choices=["1.0", "2.0", "2.1"], default="2.0")
@@ -21,39 +21,40 @@ def main():
         schema_version = comicinfo.ComicInfoSchemaVersion.V2_0
     elif args.comicinfo_schema_version == "2.1":
         schema_version = comicinfo.ComicInfoSchemaVersion.V2_1
-    for metadata_opf_file in pathlib.Path(args.path).rglob("metadata.opf"):
-        comic_info = comicinfo.ComicInfo.from_calibre_metadata_opf(metadata_opf_file, schema_version)
-        for zip_file in metadata_opf_file.parent.glob("*.zip"):
-            cbz_file = zip_file.with_suffix(".cbz")
-            with tempfile.TemporaryDirectory() as temporary_directory:
-                temporary_directory_path = pathlib.Path(temporary_directory)
-                with zipfile.ZipFile(zip_file, "r") as source_zip:
-                    source_zip.extractall(temporary_directory)
-                with zipfile.ZipFile(cbz_file, "w") as destination_cbz:
-                    images_path = pathlib.Path("images")
-                    cover = next(temporary_directory_path.glob("*/cover.*"), None)
-                    if cover is not None:
-                        destination_cbz.write(cover, images_path.joinpath(f"000000_{cover.name}"))
-                    page_count = 0
-                    for image in temporary_directory_path.glob("*/OEBPS/image/*"):
-                        destination_cbz.write(image, images_path.joinpath(image.name))
-                        page_count += 1
-                    # Set PageCount manually here since metadata.opf does not contain that information
-                    comic_info.PageCount = page_count
-                    with tempfile.NamedTemporaryFile(mode="w+", encoding="UTF-8", delete=False) as comicinfo_xml_temporary_file:
-                        comicinfo_xml_temporary_file_path = pathlib.Path(comicinfo_xml_temporary_file.name)
-                        comic_info.to_comic_info_xml(comicinfo_xml_temporary_file_path)
-                        destination_cbz.write(comicinfo_xml_temporary_file_path, "ComicInfo.xml")
-                if args.output_directory is not None:
-                    destination: pathlib.Path
-                    if args.group_by_series:
-                        destination = args.output_directory.joinpath(comic_info.Series, cbz_file.name) if comic_info.Series is not None else args.output_directory.joinpath("Other", cbz_file.name)
-                    else:
-                        destination = args.output_directory.joinpath(cbz_file.name) if args.group_by_series else args.output_directory.joinpath(cbz_file.name)
-                    destination.parent.mkdir(exist_ok=True, parents=True)
-                    if destination.exists() and destination.is_file():
-                        destination.unlink()
-                    shutil.move(cbz_file, destination)
+    for path in args.paths:
+        for metadata_opf_file in pathlib.Path(path).rglob("metadata.opf"):
+            comic_info = comicinfo.ComicInfo.from_calibre_metadata_opf(metadata_opf_file, schema_version)
+            for zip_file in metadata_opf_file.parent.glob("*.zip"):
+                cbz_file = zip_file.with_suffix(".cbz")
+                with tempfile.TemporaryDirectory() as temporary_directory:
+                    temporary_directory_path = pathlib.Path(temporary_directory)
+                    with zipfile.ZipFile(zip_file, "r") as source_zip:
+                        source_zip.extractall(temporary_directory)
+                    with zipfile.ZipFile(cbz_file, "w") as destination_cbz:
+                        images_path = pathlib.Path("images")
+                        cover = next(temporary_directory_path.glob("*/cover.*"), None)
+                        if cover is not None:
+                            destination_cbz.write(cover, images_path.joinpath(f"000000_{cover.name}"))
+                        page_count = 0
+                        for image in temporary_directory_path.glob("*/OEBPS/image/*"):
+                            destination_cbz.write(image, images_path.joinpath(image.name))
+                            page_count += 1
+                        # Set PageCount manually here since metadata.opf does not contain that information
+                        comic_info.PageCount = page_count
+                        with tempfile.NamedTemporaryFile(mode="w+", encoding="UTF-8", delete=False) as comicinfo_xml_temporary_file:
+                            comicinfo_xml_temporary_file_path = pathlib.Path(comicinfo_xml_temporary_file.name)
+                            comic_info.to_comic_info_xml(comicinfo_xml_temporary_file_path)
+                            destination_cbz.write(comicinfo_xml_temporary_file_path, "ComicInfo.xml")
+                    if args.output_directory is not None:
+                        destination: pathlib.Path
+                        if args.group_by_series:
+                            destination = args.output_directory.joinpath(comic_info.Series, cbz_file.name) if comic_info.Series is not None else args.output_directory.joinpath("Other", cbz_file.name)
+                        else:
+                            destination = args.output_directory.joinpath(cbz_file.name) if args.group_by_series else args.output_directory.joinpath(cbz_file.name)
+                        destination.parent.mkdir(exist_ok=True, parents=True)
+                        if destination.exists() and destination.is_file():
+                            destination.unlink()
+                        shutil.move(cbz_file, destination)
 
 
 if __name__ == "__main__":
