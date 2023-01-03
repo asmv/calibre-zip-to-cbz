2eeaedc93d2a318abae09c67a5253c9ae687fe43
Alex Smirnov <97alexsmirnov@gmail.com>
2023-01-02 18:15:57 -0800

    Add logging to comicinfo.py file

--- src/comicinfo.py
+++ src/comicinfo.py
@@ -6,7 +6,8 @@ import html.parser
 import functools
 import json
 import datetime
-
+import logging
+logger = logging.getLogger(__file__)
 
 XML_NAMESPACES = {
     'xs': 'http://www.w3.org/2001/XMLSchema',
@@ -86,32 +87,40 @@ class ComicInfo:
     @staticmethod
     @functools.lru_cache(maxsize=3)
     def _comicinfo_schema_attribute_names(schema_version: ComicInfoSchemaVersion) -> Set[str]:
+        logger.debug('Reading ComicInfo XSD file "%s"', schema_version.value)
         with open(pathlib.Path(__file__).parent.joinpath(schema_version.value), "r") as schema_file:
+            logger.debug('Parsing ComicInfo XSD file')
             schema = et.parse(schema_file)
             comic_info_complex_type_schema = schema.findall('./xs:complexType[@name="ComicInfo"]', XML_NAMESPACES)[0]
             return {element.attrib['name'] for element in comic_info_complex_type_schema[0] if 'name' in element.attrib}
 
     @staticmethod
     def from_calibre_metadata_opf(calibre_metadata_opf_path: pathlib.Path, schema_version: ComicInfoSchemaVersion, tags_to_genre: bool = False):
+        logger.debug('Creating ComicInfo with Schema Version: "%s"', schema_version.name.lower().replace("_", "."))
         comic_info = ComicInfo(schema_version)
+        logger.debug('Reading metadata.opf file "%s"', calibre_metadata_opf_path)
         metadata_opf: et.Element = et.parse(calibre_metadata_opf_path).getroot()
         # Set ComicInfo attributes
         ## Title
         title_element = metadata_opf.find(".//dc:title", XML_NAMESPACES)
         if title_element is not None:
             comic_info.Title = title_element.text
+            logger.debug('Set Title to: "%s"', comic_info.Title)
         ## Series
         series_element = metadata_opf.find(".//opf:meta[@name='calibre:series']", XML_NAMESPACES)
         if series_element is not None:
             comic_info.Series = series_element.attrib['content']
+            logger.debug('Set Series to: "%s"', comic_info.Series)
         ## Series Number
         series_element = metadata_opf.find(".//opf:meta[@name='calibre:series_index']", XML_NAMESPACES)
         if series_element is not None:
             comic_info.Number = series_element.attrib['content']
+            logger.debug('Set Number to: "%s"', comic_info.Number)
         ## Publisher
         publisher_element = metadata_opf.find(".//dc:publisher", XML_NAMESPACES)
         if publisher_element is not None:
             comic_info.Publisher = publisher_element.text
+            logger.debug('Set Publisher to: "%s"', comic_info.Publisher)
         ## Date
         date_element = metadata_opf.find(".//dc:date", XML_NAMESPACES)
         if date_element is not None:
@@ -119,24 +128,31 @@ class ComicInfo:
             comic_info.Year = date.year
             comic_info.Month = date.month
             comic_info.Day = date.day
+            logger.debug('Set Year to: "%i"', comic_info.Year)
+            logger.debug('Set Month to: "%i"', comic_info.Month)
+            logger.debug('Set Day to: "%i"', comic_info.Day)
         ## Language
         language_element = metadata_opf.find(".//dc:language", XML_NAMESPACES)
         if language_element is not None:
             comic_info.LanguageISO = language_element.text
+            logger.debug('Set LanguageISO to: "%s"', comic_info.LanguageISO)
         ## Description
         description_element = metadata_opf.find(".//dc:description", XML_NAMESPACES)
         if description_element is not None:
             html_parser = _HTMLDataParser()
             html_parser.feed(description_element.text)
             comic_info.Summary = html_parser.string_representation
+            logger.debug('Set Summary to: "%s"', comic_info.Summary)
         ## Tags/ Genre
         tag_elements = metadata_opf.findall(".//dc:subject", XML_NAMESPACES)
         if len(tag_elements) > 0:
             tags_comma_separated = ','.join([element.text for element in tag_elements])
             if tags_to_genre:
                 comic_info.Genre = tags_comma_separated
+                logger.debug('Set Genre to: "%s"', comic_info.Genre)
             else:
                 comic_info.Tags = tags_comma_separated
+                logger.debug('Set Tags to: "%s"', comic_info.Tags)
         ## Calibre Custom Columns (must be same case as ComicInfo tag or lowercase)
         for attribute in ComicInfo._comicinfo_schema_attribute_names(schema_version):
             custom_column_element = metadata_opf.find(f'.//opf:meta[@name="calibre:user_metadata:#{attribute}"]', XML_NAMESPACES)
@@ -145,8 +161,10 @@ class ComicInfo:
                 custom_column_element = metadata_opf.find(f'.//opf:meta[@name="calibre:user_metadata:#{attribute.lower()}"]', XML_NAMESPACES)
             if custom_column_element is not None:
                 custom_column_content_json = json.loads(custom_column_element.attrib['content'])
-                value = custom_column_content_json['#value#']
-                setattr(comic_info, attribute, ','.join(value) if type(value) is list else value)
+                custom_column_element_value = custom_column_content_json['#value#']
+                value = ','.join(custom_column_element_value) if type(custom_column_element_value) is list else custom_column_element_value
+                setattr(comic_info, attribute, value)
+                logger.debug('Set %s to: "%s"', attribute, value)
         return comic_info
 
     def to_comic_info_xml(self, xml_file: pathlib.Path):
@@ -157,5 +175,7 @@ class ComicInfo:
                 if attrib in comic_info_schema_attributes and value is not None:
                     element = et.Element(attrib)
                     element.text = str(value)
+                    logger.debug('Adding attribute %s as chile to ComicInfo', attrib)
                     comic_info.insert(0, element)
+        logger.debug('Writing resulting ComicInfo XML to file "%s"', xml_file)
         et.ElementTree.write(et.ElementTree(comic_info), xml_file, xml_declaration=True, encoding='UTF-8', method='xml', short_empty_elements=False)
